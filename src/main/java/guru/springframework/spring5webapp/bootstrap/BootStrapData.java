723694877e7a18020752e6976b78f24dfc2067ce
bipin247 <bipin247@gmail.com>
2020-09-29 16:00:35 +0100

    new bootstrap files

---
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -0,0 +1,23 @@
+package guru.springframework.spring5webapp.bootstrap;
+
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BootStrapData implements CommandLineRunner {
+
+    private final BookRepository bookRepository;
+    private final AuthorRepository authorRepository;
+
+    public BootStrapData(BookRepository bookRepository, AuthorRepository authorRepository) {
+        this.bookRepository = bookRepository;
+        this.authorRepository = authorRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
