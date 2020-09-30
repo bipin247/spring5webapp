d7a8e06ed1c74f9d8b655fb9991b1f03cc1c6d55
bipin247 <bipin247@gmail.com>
2020-09-30 09:14:59 +0100

    new bootstrapData files

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.model.Author;
 import guru.springframework.spring5webapp.model.Book;
+import guru.springframework.spring5webapp.model.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -15,10 +17,12 @@ public class BootStrapData implements CommandLineRunner {
 
     private final BookRepository bookRepository;
     private final AuthorRepository authorRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootStrapData(BookRepository bookRepository, AuthorRepository authorRepository) {
+    public BootStrapData(BookRepository bookRepository, AuthorRepository authorRepository, PublisherRepository publisherRepository) {
         this.bookRepository = bookRepository;
         this.authorRepository = authorRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -40,8 +44,12 @@ public class BootStrapData implements CommandLineRunner {
         bookRepository.save(missionImpossible);
         bookRepository.save(inception);
 
+        Publisher leamington  = new Publisher("world books","9 highfield", "richmond","texas","77404");
+        publisherRepository.save(leamington);
+
         System.out.println("started in boostrap");
         System.out.println("number of books added " + bookRepository.count());
         System.out.println("number of authors added " + authorRepository.count());
+        System.out.println("number of publishers added " + publisherRepository.count());
     }
 }
